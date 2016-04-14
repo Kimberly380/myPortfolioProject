7777d4e60a5ab28e431314b22f47e10aeff3ffbc
Kim Feinberg <kimberly380@gmail.com>
2016-04-13 21:23:17 -0700

    made page repsonsive, mobile first, then desktop.  Updated readme.  Added font awesome icons.

--- JS/nav.js
+++ JS/nav.js
@@ -2,9 +2,12 @@
 
 //hide all but home on load
 
-$('section').filter('section').not('.home').hide();
+function onLoad (){
+    $('section').filter('section').not('.home').hide();
+}
+onLoad();
 
-//show hide by nav bar
+//show hide by nav bar (TODO fix to use data-* tag instead of id/class tags)
 $(function() {
     var ids = '';
     var listItems = $('li');
@@ -15,3 +18,12 @@ $(function() {
         $('section').filter('section').not("."+ids).hide();
     });
 });
+
+
+//don't know why the below code does not work.  Going to leave it out for now.
+// $('#hamburger').hover(function(){
+//     $('nav ul').css('display','flex');
+//     }, function(){
+//     $('nav ul').css('display','none');
+//     }
+// );
